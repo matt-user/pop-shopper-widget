363f3acbd0ccb4b0f8d049d9e2719c71f4942c5d
Matt Auer <mattauer@umich.edu>
2025-05-15 22:20:57 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import "./widget/styles/style.css"
+
+import { WidgetContainer } from "./widget/components/widget-container"
+
+function App() {
+
+  return (
+    <>
+     <WidgetContainer clientKey="test-key" />
+    </>
+  )
+}
+
+export default App
